f79572cf2b76d6dcc06a16d36b87716469f359e9
ishikawa shinya <shinya.ishikawa@drecom.co.jp>
2016-03-11 18:52:32 +0900

    rewrite debug script

--- git_debug.rb
+++ git_debug.rb
@@ -1,21 +1,52 @@
 require "git"
+require "logger"
 
-line = "#{Time.now}: Updated"
-`echo #{line} >> README.md`
-
-g = Git.open(ENV["PWD"])
-
-g.add("README.md")
-g.commit("[DEBUG] modified")
-
-tag_name = "1.0.0"
-if tag = g.tags.find { |t| t.name == tag_name }
-  g.delete_tag(tag.name)
+def log(message)
+  $stderr.puts("** #{message}")
 end
-g.add_tag(tag_name)
 
-tags = Git.ls_remote().fetch("tags")
-if tags.has_key?(tag_name)
-  g.push("origin", ":" + tag_name)
+def create_tag(version)
+  if version.empty?
+    log("VERSION is empty")
+    return
+  end
+
+  g = Git.open(ENV["PWD"])
+  g.fetch("origin")
+
+  tag_name = "v#{version}"
+  tag = g.tags.find { |t| t.name == tag_name }
+  if tag
+    log("#{tag.name}@local already exists")
+  else
+    tag = g.add_tag(tag_name)
+    log("#{tag_name}@local was created")
+  end
+
+  tags = Git.ls_remote("origin").fetch("tags", {})
+  if tags.has_key?(tag.name)
+    log("#{tag.name}@remote already exists")
+    return
+  end
+
+  print "Do you push #{tag.name} ? [y/n]> "
+  answer = $stdin.readline.strip.downcase
+  unless answer == "y"
+    log("CANCELED")
+    return
+  end
+  g.push("origin", tag.name)
+  log("PUSHED")
+
+  tag
 end
-g.push("origin", tag_name)
+
+def main
+  version = `cat VERSION`.strip
+  unless create_tag(version)
+    log("ABORT")
+    exit(1)
+  end
+end
+
+main
